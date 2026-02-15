b305e735eab8302d9ee58ccf7d796bf0c9df1479
baldlow <147939873+baldlow@users.noreply.github.com>
2026-02-14 22:44:28 -0500

    added simple cli, check cpu usage over x seconds

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use chrono::{TimeDelta, prelude::*};
+use clap::Parser;
+use sysinfo::System;
+
+#[derive(Parser)]
+#[command(version, about, long_about= None)]
+struct Cli {
+    // type should be the cpu
+    #[arg(short, long)]
+    system_resource: String,
+
+    #[arg(short, long, default_value_t = 1)]
+    time_seconds: i64,
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!(
+        "pattern: {:?}, path: {:?}",
+        args.system_resource, args.time_seconds
+    );
+    let start_time = Utc::now().time();
+    let mut end_time = Utc::now().time();
+    let mut sys = System::new();
+    let mut diff = end_time - start_time;
+    let time_delta = TimeDelta::seconds(args.time_seconds);
+    if args.system_resource == "cpu" {
+        sys.refresh_cpu_usage();
+        while diff < time_delta {
+            end_time = Utc::now().time();
+            diff = end_time - start_time;
+            for cpu in sys.cpus() {
+                println!("{}%", cpu.cpu_usage());
+            }
+        }
+    }
 }
