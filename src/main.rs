013497cd20e99edcc2c3114a7336a4a4936e34c8
baldlow <theb3stn0scoper555@gmail.com>
2026-02-16 13:16:45 -0500

    added way to view mem usage over x seconds

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use chrono::{TimeDelta, prelude::*};
 use clap::Parser;
+use num::pow;
 use sysinfo::System;
 
 #[derive(Parser)]
@@ -13,6 +14,7 @@ struct Cli {
     time_seconds: i64,
 }
 fn main() {
+    // parges user input
     let args = Cli::parse();
     println!(
         "pattern: {:?}, path: {:?}",
@@ -23,14 +25,30 @@ fn main() {
     let mut sys = System::new();
     let mut diff = end_time - start_time;
     let time_delta = TimeDelta::seconds(args.time_seconds);
+
     if args.system_resource == "cpu" {
-        sys.refresh_cpu_usage();
         while diff < time_delta {
+            sys.refresh_cpu_usage();
             end_time = Utc::now().time();
             diff = end_time - start_time;
             for cpu in sys.cpus() {
                 println!("{}%", cpu.cpu_usage());
             }
+            std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
+        }
+    } else if args.system_resource == "mem" {
+        while diff < time_delta {
+            // only refresh ram
+            sys.refresh_memory_specifics(sysinfo::MemoryRefreshKind::everything().with_ram());
+            end_time = Utc::now().time();
+            diff = end_time - start_time;
+            let mem_in_bytes = sys.used_memory();
+            let kb: u64 = 1000;
+            let divisor = num::pow(kb, 3);
+
+            // to get decimal on prints
+            let mem_in_gigabytes = (mem_in_bytes as f64) / (divisor as f64);
+            println!("mem usage {} gb", mem_in_gigabytes);
         }
     }
 }
