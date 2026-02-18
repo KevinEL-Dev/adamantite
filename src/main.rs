5707326b5680f15ec87aa960799e86fc86cfe6dc
baldlow <theb3stn0scoper555@gmail.com>
2026-02-18 14:46:40 -0500

    added % on debug output for hytale_usuage

--- src/main.rs
+++ src/main.rs
@@ -150,7 +150,7 @@ fn get_hytale_total_cpu_usage(hytale_pid: u32) {
         .wait_with_output()
         .expect("failed to wait for awk");
     println!(
-        "output from awk\n {}",
-        String::from_utf8_lossy(&output.stdout)
+        "output from awk\n{}%",
+        String::from_utf8_lossy(&output.stdout).trim()
     );
 }
