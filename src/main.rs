9831b83ede62d187841ddf43ff46635b9726fdcc
kevin l <dev@zookfish.com>
2026-02-28 22:07:24 -0500

    added pressure subcommand option

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ use std::io::prelude::*;
 use std::path::Path;
 use std::process::{Command, Stdio};
 use sysinfo::{Disks, Pid, ProcessRefreshKind, ProcessesToUpdate, System};
+const LOW_IO_PRESSURE_MAX: f64 = 1.0;
+const MODERATE_IO_PRESSURE_MAX: f64 = 1.0;
+const HIGH_IO_PRESSURE_MAX: f64 = 1.0;
+
 /// Track system resources over time
 #[derive(Parser)]
 #[command(version, about, long_about= None)]
@@ -21,6 +25,13 @@ enum SystemResource {
     /// System resource memory
     Mem,
 }
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum PressureType {
+    /// Pressure type Io
+    Io,
+    /// Pressure type Mem
+    Mem,
+}
 #[derive(Subcommand)]
 enum Commands {
     /// Tracks a system resource
@@ -29,6 +40,11 @@ enum Commands {
         system_resource: SystemResource,
         #[arg(short, long, default_value_t = 1)]
         time_seconds: i64,
+    },
+    /// Shows how often system work is stalled due to resource contention
+    Pressure {
+        #[arg(value_enum)]
+        pressure_type: PressureType,
     },
 }
 struct Pressure {
@@ -69,6 +85,19 @@ impl Pressure {
             full: map_full,
         }
     }
+    fn evaluate_pressure(&self) {
+        let avg300_val = self.some.get("avg300");
+        match avg300_val {
+            Some(val) => {
+                if *val < LOW_IO_PRESSURE_MAX {
+                    println!("Disk pressure is healthy ({}% sustained)", val);
+                } else if *val >= MODERATE_IO_PRESSURE_MAX {
+                    println!("Disk pressure is concerning ({}% sustained)", val);
+                }
+            }
+            None => println!("failed to find the avg300_val"),
+        }
+    }
 }
 fn main() {
     // parges user input
@@ -80,11 +109,14 @@ fn main() {
     let mut user_selected_time = 1;
     let mut time_delta = TimeDelta::seconds(user_selected_time);
     let mut sys_resource = SystemResource::Cpu;
+    sys.refresh_all();
+    let num_of_cpus = sys.cpus().len();
+    let hytale_pid = find_pid_of_hytale();
+    let num_of_cpus = num_of_cpus as f32;
     /*let disks = Disks::new_with_refreshed_list();
     for disk in disks.list() {
         println!("[{:?}] {:?}", disk.name(), disk.usage());
     }*/
-    show_system_pressure();
     match &args.command {
         Some(Commands::Track {
             system_resource,
@@ -94,136 +126,120 @@ fn main() {
                 time_delta = TimeDelta::seconds(*time_seconds);
                 sys_resource = *system_resource;
                 user_selected_time = *time_seconds;
+                let mut counter = 0;
+                let mut hytale_total_cpu_usage: f32 = 0.0;
+                let mut total_system_cpu_usage: f32 = 0.0;
+                let total_available_cpu_percentage: f32 = num_of_cpus * 100.0;
+                while diff < time_delta {
+                    sys.refresh_cpu_usage();
+                    let mut current_system_cpu_usage: f32 = 0.0;
+                    end_time = Utc::now().time();
+                    diff = end_time - start_time;
+                    for cpu in sys.cpus() {
+                        current_system_cpu_usage += cpu.cpu_usage();
+                    }
+                    total_system_cpu_usage +=
+                        current_system_cpu_usage / total_available_cpu_percentage;
+
+                    let hytale_curr_cpu_usage = get_cpu_usage_from_pid(hytale_pid);
+
+                    hytale_total_cpu_usage += hytale_curr_cpu_usage;
+                    counter += 1;
+                    std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
+                }
+                println!(
+                    "For {} seconds hytale on average used {:.2} cpus",
+                    user_selected_time,
+                    ((hytale_total_cpu_usage / counter as f32) / total_available_cpu_percentage)
+                        * num_of_cpus
+                );
+                println!(
+                    "For {} seconds your system on average used {:.2} cpus",
+                    user_selected_time,
+                    (total_system_cpu_usage / counter as f32) * (num_of_cpus)
+                );
+                println!(
+                    "For {} seconds your system on average was at {:.2}% load",
+                    user_selected_time,
+                    (total_system_cpu_usage / counter as f32) * 100.0
+                );
+                println!(
+                    "For {} seconds hytale on average used {:.2}% load",
+                    user_selected_time,
+                    ((hytale_total_cpu_usage / counter as f32) / total_available_cpu_percentage)
+                        * 100.0
+                );
             }
             SystemResource::Mem => {
                 time_delta = TimeDelta::seconds(*time_seconds);
                 sys_resource = *system_resource;
                 user_selected_time = *time_seconds;
+                let system_total_memory = sys.total_memory();
+
+                let mut total_mem_usage_in_bytes = 0;
+                let mut total_hytale_mem_usage_in_bytes = 0;
+                let mut counter = 0;
+                while diff < time_delta {
+                    // only refresh ram
+                    sys.refresh_memory_specifics(
+                        sysinfo::MemoryRefreshKind::everything().with_ram(),
+                    );
+                    end_time = Utc::now().time();
+                    diff = end_time - start_time;
+                    let curr_system_mem_in_bytes = sys.used_memory();
+                    let curr_hytale_mem_in_bytes = get_mem_usage_from_pid(hytale_pid);
+                    total_mem_usage_in_bytes += curr_system_mem_in_bytes;
+                    total_hytale_mem_usage_in_bytes += curr_hytale_mem_in_bytes;
+                    counter += 1;
+                }
+                let total_mem_in_gigabytes =
+                    return_mem_in_gigabytes(total_mem_usage_in_bytes as f64);
+                let total_hytale_mem_in_gigabytes =
+                    return_mem_in_gigabytes(total_hytale_mem_usage_in_bytes as f64);
+                let average_hytale_mem_usage_gb = total_hytale_mem_in_gigabytes / counter as f64;
+
+                println!(
+                    "Average mem usage of hytale over {} seconds is {:.2} gb.",
+                    user_selected_time, average_hytale_mem_usage_gb
+                );
+                let average_system_mem_usage_gb = total_mem_in_gigabytes / counter as f64;
+
+                println!(
+                    "Average mem usage of entire system over {} seconds is {:.2} gb.",
+                    user_selected_time, average_system_mem_usage_gb
+                );
+                let average_hytale_mem_usage = return_mem_usage(
+                    total_hytale_mem_usage_in_bytes as f64 / counter as f64,
+                    system_total_memory as f64,
+                );
+                println!(
+                    "Average mem usage in percentage for hytale over {} seconds is {:.2}%",
+                    user_selected_time, average_hytale_mem_usage
+                );
+                let average_system_mem_usage = return_mem_usage(
+                    total_mem_usage_in_bytes as f64 / counter as f64,
+                    system_total_memory as f64,
+                );
+                println!(
+                    "Average mem usage in percentage for entire system over {} seconds is {:.2}%",
+                    user_selected_time, average_system_mem_usage
+                );
+                println!(
+                    "Average mem usage in percentage for entire system without hytale processes over {} seconds is {:.2}%",
+                    user_selected_time,
+                    average_system_mem_usage - average_hytale_mem_usage
+                );
+            }
+        },
+        Some(Commands::Pressure { pressure_type }) => match pressure_type {
+            PressureType::Io => {
+                show_system_pressure(PressureType::Io);
+            }
+            PressureType::Mem => {
+                show_system_pressure(PressureType::Mem);
             }
         },
         None => {}
-    }
-
-    sys.refresh_all();
-    let num_of_cpus = sys.cpus().len();
-    let hytale_pid = find_pid_of_hytale();
-    let num_of_cpus = num_of_cpus as f32;
-    if sys_resource == SystemResource::Cpu {
-        let mut counter = 0;
-        let mut hytale_total_cpu_usage: f32 = 0.0;
-        let mut total_system_cpu_usage: f32 = 0.0;
-        let total_available_cpu_percentage: f32 = num_of_cpus * 100.0;
-        while diff < time_delta {
-            sys.refresh_cpu_usage();
-            let mut current_system_cpu_usage: f32 = 0.0;
-            end_time = Utc::now().time();
-            diff = end_time - start_time;
-            for cpu in sys.cpus() {
-                current_system_cpu_usage += cpu.cpu_usage();
-            }
-            total_system_cpu_usage += current_system_cpu_usage / total_available_cpu_percentage;
-
-            let hytale_curr_cpu_usage = get_cpu_usage_from_pid(hytale_pid);
-
-            hytale_total_cpu_usage += hytale_curr_cpu_usage;
-            counter += 1;
-            std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
-        }
-        println!(
-            "For {} seconds hytale on average used {:.2} cpus",
-            user_selected_time,
-            ((hytale_total_cpu_usage / counter as f32) / total_available_cpu_percentage)
-                * num_of_cpus
-        );
-        println!(
-            "For {} seconds your system on average used {:.2} cpus",
-            user_selected_time,
-            (total_system_cpu_usage / counter as f32) * (num_of_cpus)
-        );
-        println!(
-            "For {} seconds your system on average was at {:.2}% load",
-            user_selected_time,
-            (total_system_cpu_usage / counter as f32) * 100.0
-        );
-        println!(
-            "For {} seconds hytale on average used {:.2}% load",
-            user_selected_time,
-            ((hytale_total_cpu_usage / counter as f32) / total_available_cpu_percentage) * 100.0
-        );
-    } else if sys_resource == SystemResource::Mem {
-        let system_total_memory = sys.total_memory();
-
-        let mut total_mem_usage_in_bytes = 0;
-        let mut total_hytale_mem_usage_in_bytes = 0;
-        let mut counter = 0;
-        while diff < time_delta {
-            // only refresh ram
-            sys.refresh_memory_specifics(sysinfo::MemoryRefreshKind::everything().with_ram());
-            end_time = Utc::now().time();
-            diff = end_time - start_time;
-            let curr_system_mem_in_bytes = sys.used_memory();
-            let curr_hytale_mem_in_bytes = get_mem_usage_from_pid(hytale_pid);
-            total_mem_usage_in_bytes += curr_system_mem_in_bytes;
-            total_hytale_mem_usage_in_bytes += curr_hytale_mem_in_bytes;
-            counter += 1;
-        }
-        let total_mem_in_gigabytes = return_mem_in_gigabytes(total_mem_usage_in_bytes as f64);
-        let total_hytale_mem_in_gigabytes =
-            return_mem_in_gigabytes(total_hytale_mem_usage_in_bytes as f64);
-        let average_hytale_mem_usage_gb = total_hytale_mem_in_gigabytes / counter as f64;
-
-        println!(
-            "Average mem usage of hytale over {} seconds is {:.2} gb.",
-            user_selected_time, average_hytale_mem_usage_gb
-        );
-        let average_system_mem_usage_gb = total_mem_in_gigabytes / counter as f64;
-
-        println!(
-            "Average mem usage of entire system over {} seconds is {:.2} gb.",
-            user_selected_time, average_system_mem_usage_gb
-        );
-        let average_hytale_mem_usage = return_mem_usage(
-            total_hytale_mem_usage_in_bytes as f64 / counter as f64,
-            system_total_memory as f64,
-        );
-        println!(
-            "Average mem usage in percentage for hytale over {} seconds is {:.2}%",
-            user_selected_time, average_hytale_mem_usage
-        );
-        let average_system_mem_usage = return_mem_usage(
-            total_mem_usage_in_bytes as f64 / counter as f64,
-            system_total_memory as f64,
-        );
-        println!(
-            "Average mem usage in percentage for entire system over {} seconds is {:.2}%",
-            user_selected_time, average_system_mem_usage
-        );
-        println!(
-            "Average mem usage in percentage for entire system without hytale processes over {} seconds is {:.2}%",
-            user_selected_time,
-            average_system_mem_usage - average_hytale_mem_usage
-        );
-    }
-    /* else if args.system_resource == "net" {
-        let mut networks = Networks::new_with_refreshed_list();
-        println!("=> networks:");
-        while diff < time_delta {
-            networks.refresh(true);
-            end_time = Utc::now().time();
-            diff = end_time - start_time;
-            for (interface_name, data) in &networks {
-                println!(
-                    "{interface_name}: {} B (down) / {} B (up)",
-                    data.total_received(),
-                    data.total_transmitted(),
-                );
-            }
-        }
-    }*/
-    else {
-        println!(
-            "Please input a valid system resource. Only current valid resources are \"cpu\" and \"mem\""
-        );
     }
 }
 fn find_pid_of_hytale() -> u32 {
@@ -303,13 +319,13 @@ fn return_mem_in_gigabytes(mem_in_bytes: f64) -> f64 {
 fn return_mem_usage(mem_in_bytes: f64, system_total_mem_in_bytes: f64) -> f64 {
     (mem_in_bytes / system_total_mem_in_bytes) * 100.0
 }
-fn show_system_pressure() {
+fn show_system_pressure(pressure_type: PressureType) {
     let path_to_psi_io = Path::new("/proc/pressure/io");
     let path_to_psi_mem = Path::new("/proc/pressure/memory");
 
     let mut f = File::open(path_to_psi_io).expect("failed to open this file");
-    let mut content = String::new();
-    f.read_to_string(&mut content)
+    let mut content_io = String::new();
+    f.read_to_string(&mut content_io)
         .expect("failed to read the file");
     let mut f_memory = File::open(path_to_psi_mem).expect("failed to open file memory");
     let mut content_memory = String::new();
@@ -317,17 +333,22 @@ fn show_system_pressure() {
         .read_to_string(&mut content_memory)
         .expect("failed to read file memory");
 
-    let v_f: Vec<&str> = content.lines().collect();
+    let v_f: Vec<&str> = content_io.lines().collect();
     let v_f_some: Vec<&str> = v_f[0].split_whitespace().collect();
     let v_f_full: Vec<&str> = v_f[1].split_whitespace().collect();
 
-    let pressure_example = Pressure::build_pressure(v_f_some, v_f_full);
-    println!("hashmap generated for some");
-    for (key, value) in pressure_example.some.iter() {
-        println!("key: {key} val: {value}");
-    }
-    println!("hashmap generated for full");
-    for (key, value) in pressure_example.some.iter() {
-        println!("key: {key} val: {value}");
+    let v_f_mem: Vec<&str> = content_memory.lines().collect();
+    let v_f_mem_some: Vec<&str> = v_f_mem[0].split_whitespace().collect();
+    let v_f_mem_full: Vec<&str> = v_f_mem[1].split_whitespace().collect();
+
+    match pressure_type {
+        PressureType::Io => {
+            let pressure_example = Pressure::build_pressure(v_f_some, v_f_full);
+            pressure_example.evaluate_pressure();
+        }
+        PressureType::Mem => {
+            let pressure_example = Pressure::build_pressure(v_f_mem_some, v_f_mem_full);
+            pressure_example.evaluate_pressure();
+        }
     }
 }
