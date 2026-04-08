998dd4864c47f129f2eec9a175073b1bc9791591
kevin l <dev@zookfish.com>
2026-04-08 19:15:12 -0400

    make sure print comes before output

--- src/main.rs
+++ src/main.rs
@@ -624,8 +624,8 @@ fn find_pid_of_hytale() -> u32 {
                     .expect("failed to start systemd");
                 let ecode = systemd_cgls_child.wait().expect("failed to wait on ps");
                 if ecode.success() == false{
-                    let systemd_cgls_out = systemd_cgls_child.wait_with_output().expect("failed to get systemd-cgls output");
                     println!("Systemd-cgls failed, error below");
+                    let systemd_cgls_out = systemd_cgls_child.wait_with_output().expect("failed to get systemd-cgls output");
                     print!("{}",String::from_utf8_lossy(&systemd_cgls_out.stdout));
                     process::exit(1)
                 }
