b8cda7a3670dc983a7a84be2119773f580c96d7a
kevin l <dev@zookfish.com>
2026-04-08 14:02:41 -0400

    added to pipe to stdout

--- src/main.rs
+++ src/main.rs
@@ -626,6 +626,7 @@ fn find_pid_of_hytale(settings: Map<String,Value>) -> u32 {
                 .arg("-u")
                 .arg(name)
                 .arg("--no-pager")
+                .stdout(Stdio::piped())
                 .spawn()
                 .expect("failed to start systemd");
             systemd_cgls_child.wait().expect("failed to wain on systemd");
