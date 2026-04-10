222468eebd6aeefc012ec525019312d81b8425ef
kevin l <dev@zookfish.com>
2026-04-10 17:31:31 -0400

    update build-script with correct paths

--- build-script.sh
+++ build-script.sh
@@ -10,6 +10,6 @@ cp target/release/adamantite dist/adamantite-v${version}-unknown-linux-gnu/
 cp README.md dist/adamantite-v${version}-unknown-linux-gnu/
 cp LICENSE dist/adamantite-v${version}-unknown-linux-gnu/
 
-tar czf adamantite-v${version}-unknown-linux-gnu.tar.gz adamantite-v${version}-unknown-linux-gnu/
+tar czf dist/adamantite-v${version}-unknown-linux-gnu.tar.gz dist/adamantite-v${version}-unknown-linux-gnu/
 
 sha256sum dist/adamantite-v${version}-unknown-linux-gnu.tar.gz > dist/SHA256SUMS 
